d85bf7b1bb8867577af16cfa9eb39efc0250d658
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-26 16:56:09 -0400

    feat: :sparkles: add pipe phone function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-import { Page1Component } from './shared/pages/page-1/page-1.component';
-import { Page2Component } from './shared/pages/page-2/page-2.component';
-import { NotfoundComponent } from './shared/components/notfound/notfound.component';
+import { Page1Component } from "./shared/pages/page-1/page-1.component";
+import { Page2Component } from "./shared/pages/page-2/page-2.component";
+import { NotfoundComponent } from "./shared/components/notfound/notfound.component";
+
+import { PhonePipe } from "./shared/pipes/client.pipe";
 
 @NgModule({
-    declarations: [AppComponent, Page1Component, Page2Component, NotfoundComponent],
+    declarations: [
+        AppComponent,
+        Page1Component,
+        Page2Component,
+        NotfoundComponent,
+        PhonePipe,
+    ],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
