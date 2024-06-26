24902bbbc36225115a6b63bf6c956516e9a70882
Diego <gonzalez.rojas.ignacio.diego@gmail.com>
2024-06-26 16:20:21 -0400

    feat: :sparkles: console log data from api sing metodhs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ClienteService } from "./core/services/client.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'app';
+    title = "app";
+
+    constructor(private clienteService: ClienteService) {
+        this.getClients();
+        this.getClientsSp();
+    }
+
+    getClients(): void {
+        this.clienteService.getClients(1, 10).subscribe(
+            (clients) => {
+                console.log("clients no sp ", clients);
+            },
+            (error) => {
+                console.error("Error al obtener clientes:", error);
+            }
+        );
+    }
+
+    getClientsSp(): void {
+        this.clienteService.getClientsSp(1, 10).subscribe(
+            (clients) => {
+                console.log("clientes sp ", clients);
+            },
+            (error) => {
+                console.error(
+                    "Error al obtener clientes usando procedimiento almacenado:",
+                    error
+                );
+            }
+        );
+    }
 }
